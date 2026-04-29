7a3312bd68d0cf42d40584a926446d56f4ef3505
Vinicius Vilas Boas <boas.vini@gmail.com>
2026-04-28 22:41:31 -0300

    feat: adiciona os deferenciais - tratamento global de excecoes e logging estruturado

--- src/main/java/com/fiap/reserva_salas/api/service/ReservaService.java
+++ src/main/java/com/fiap/reserva_salas/api/service/ReservaService.java
@@ -5,10 +5,12 @@ import com.fiap.reserva_salas.api.domain.Sala;
 import com.fiap.reserva_salas.api.dto.CriarReservaDTO;
 import com.fiap.reserva_salas.api.repository.ReservaRepository;
 import com.fiap.reserva_salas.api.repository.SalaRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class ReservaService {
 
@@ -21,16 +23,20 @@ public class ReservaService {
     }
 
     public Reserva criar(CriarReservaDTO dto) {
+        log.info("Iniciando criacao de reserva para a sala ID: {}", dto.getSalaId()); // <-- LOG AQUI
+
         Sala sala = salaRepo.findById(dto.getSalaId())
                 .orElseThrow(() -> new RuntimeException("Sala não encontrada!"));
 
         if (dto.getDataHoraInicio().isAfter(dto.getDataHoraFim()) || dto.getDataHoraInicio().isEqual(dto.getDataHoraFim())) {
+            log.warn("Tentativa de reserva com datas invalidas pelo usuario: {}", dto.getResponsavel()); // <-- LOG AQUI
             throw new IllegalArgumentException("A data de início deve ser anterior à data de fim.");
         }
 
         boolean temConflito = reservaRepo.existeConflito(dto.getSalaId(), dto.getDataHoraInicio(), dto.getDataHoraFim());
 
         if (temConflito) {
+            log.error("Conflito detectado para a sala {} no periodo selecionado", sala.getNome()); // <-- LOG AQUI
             throw new RuntimeException("Conflito de horários: Já existe uma reserva para esta sala neste período.");
         }
 
@@ -40,10 +46,12 @@ public class ReservaService {
         reserva.setDataHoraInicio(dto.getDataHoraInicio());
         reserva.setDataHoraFim(dto.getDataHoraFim());
 
+        log.info("Reserva criada com sucesso no banco de dados!"); // <-- LOG AQUI
         return reservaRepo.save(reserva);
     }
 
     public List<Reserva> listar() {
+        log.info("Buscando todas as reservas cadastradas...");
         return reservaRepo.findAll();
     }
 
@@ -51,6 +59,7 @@ public class ReservaService {
         if (!reservaRepo.existsById(id)) {
             throw new RuntimeException("Reserva não encontrada!");
         }
+        log.info("Cancelando reserva de ID: {}", id);
         reservaRepo.deleteById(id);
     }
 }
